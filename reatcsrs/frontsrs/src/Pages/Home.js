55179ec77e33ff44f21921642564adea2d79802c
kenza dahibi <kenzadahibi@gmail.com>
2023-07-17 15:02:12 +0100

    commit4

--- reatcsrs/frontsrs/src/Pages/Home.js
+++ reatcsrs/frontsrs/src/Pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Navbar from '../SharedComponent/Navbar/Navbar'
-import Container from '../SharedComponent/CenterContainer/Container'
+
 
  const Home = () => {
   return (
     <div>
-    <Navbar></Navbar>
-    <Container></Container>
+    
 
     </div>
   )
